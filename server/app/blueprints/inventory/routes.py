cc993b57d7feacc0a9c8de7f7d7a5a17a3c6d2ef
Grigorios Alexandrou <t8220004@aueb.gr>
2025-05-24 19:55:05 +0300

    Fix issue inventory

--- server/app/blueprints/inventory/routes.py
+++ server/app/blueprints/inventory/routes.py
@@ -8,7 +8,19 @@ def inventory():
     """
     Endpoint to get the inventory data.
     """
-    result = getInventory()
+    result = getInventory(dropdown=False)
+
+    if result is None:
+        return jsonify({"error": "No inventory data found"}), 404
+    else:
+        return jsonify(result), 200
+    
+@inventory_bp.route('/inventory-dropdown', methods=['GET'])
+def inventory_dropdown():
+    """
+    Endpoint to get the inventory data.
+    """
+    result = getInventory(dropdown=True)
 
     if result is None:
         return jsonify({"error": "No inventory data found"}), 404
